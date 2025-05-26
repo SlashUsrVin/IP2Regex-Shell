f5276347fc6fd2c88192ae96473e94c518948f59
Vin <m.vin.321@outlook.com>
2025-05-27 01:17:15 +0800

    updated license from GPLv3 to MIT

--- ip2regex.sh
+++ ip2regex.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # IP2Regex - Converts ip range to regex
-# Copyright (C) 2025 https://github.com/SlashUsrVin
+# Author: SlashUsrVin
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# MIT License
+# Copyright (c) 2025 SlashUsrVin
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# See the LICENSE file in the repository root for full text.
 
 #SAMPLE INPUT: 192.168.1.0 192.168.1.255
 #SAMPLE OUTPUT: (192)\.(168)\.(1)\.([0-9]|[2-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])
